094bdbd90932711162e3be24496ad4e5dff87fff
maqiang <maqiangddb@gmail.com>
2013-12-29 22:37:50 +0800

    get sensor data and show

--- README.md
+++ README.md
@@ -15,3 +15,23 @@ an receive blue data application
 
 手机传感器数据接收模块。
 传感器数据处理算法模块。
+
+ sensors count:8
+=[AK8973 3-axis Accelerometer,Asahi Kasei,0.2,39.24,0.15328126,0]
+=[AK8973 3-axis Magnetic field sensor,Asahi Kasei,6.8,2000.0,0.0625,0]
+=[AK8973 Orientation sensor,Asahi Kasei,7.0,360.0,1.0,0]
+=[Taos Proximity sensor,Taos Inc.,0.5,5.0,5.0,0]
+=[Taos Light sensor,Taos Inc.,0.5,27000.0,1.0,0]
+=[Gravity Sensor,Google Inc.,0.2,39.24,0.15328126,0]
+=[Linear Acceleration Sensor,Google Inc.,0.2,39.24,0.15328126,0]
+=[Rotation Vector Sensor,Google Inc.,7.0,1.0,5.9604645E-8,0]
+
+12-28 14:52:44.080    5894-5894/com.some.blue I/com.some.blue﹕ sensors count:8
+12-28 14:52:44.090    5894-5894/com.some.blue I/com.some.blue﹕ ==[AK8973 3-axis Accelerometer,Asahi Kasei,0.2,39.24,0.15328126,0]
+12-28 14:52:44.100    5894-5894/com.some.blue I/com.some.blue﹕ ==[AK8973 3-axis Magnetic field sensor,Asahi Kasei,6.8,2000.0,0.0625,0]
+12-28 14:52:44.100    5894-5894/com.some.blue I/com.some.blue﹕ ==[AK8973 Orientation sensor,Asahi Kasei,7.0,360.0,1.0,0]
+12-28 14:52:44.100    5894-5894/com.some.blue I/com.some.blue﹕ ==[Taos Proximity sensor,Taos Inc.,0.5,5.0,5.0,0]
+12-28 14:52:44.150    5894-5894/com.some.blue I/com.some.blue﹕ ==[Taos Light sensor,Taos Inc.,0.5,27000.0,1.0,0]
+12-28 14:52:44.150    5894-5894/com.some.blue I/com.some.blue﹕ ==[Gravity Sensor,Google Inc.,0.2,39.24,0.15328126,0]
+12-28 14:52:44.150    5894-5894/com.some.blue I/com.some.blue﹕ ==[Linear Acceleration Sensor,Google Inc.,0.2,39.24,0.15328126,0]
+12-28 14:52:44.150    5894-5894/com.some.blue I/com.some.blue﹕ ==[Rotation Vector Sensor,Google Inc.,7.0,1.0,5.9604645E-8,0]
--- src/com/some/blue/MainActivity.java
+++ src/com/some/blue/MainActivity.java
@@ -6,20 +6,32 @@ import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
 import java.util.Set;
 
-public class MainActivity extends Activity
+public class MainActivity extends Activity implements SensorEventListener
 {
 
     BroadcastReceiver _receiver;
     TextView debug;
     BluetoothAdapter adapter;
     SensorManager sensorManager;
+
+    TextView acc_data;
+    TextView gra_data;
+    TextView gyro_data;
+    TextView light_data;
+    TextView mag_data;
+    Button stop;
+    Button start;
 
 
     /** Called when the activity is first created. */
@@ -29,6 +41,7 @@ public class MainActivity extends Activity
         Util.LOGI("==========================onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        initUI();
         debug = (TextView) findViewById(R.id.debug);
 
         adapter = BluetoothAdapter.getDefaultAdapter();
@@ -46,9 +59,39 @@ public class MainActivity extends Activity
         List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
         Util.LOGI("sensors count:"+sensors.size());
         for (Sensor sensor : sensors) {
-           Util.LOGI("==["+sensor.getName()+"]");
+           Util.LOGI("==[name:"+sensor.getName()+","
+                                    +"type:"+sensor.getType()+","
+                                    +"vendor"+sensor.getVendor()+","
+                                    +"power:"+sensor.getPower()+","
+                                    +"maximumRange:"+sensor.getMaximumRange()+","
+                                    +"resolution:"+sensor.getResolution()+","
+                                    +"minDelay:"+sensor.getMinDelay()
+                                    +"]");
+           sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
         }
 
+    }
+
+    private void initUI() {
+        acc_data = (TextView) findViewById(R.id.acc_data);
+        gra_data = (TextView) findViewById(R.id.gra_data);
+        gyro_data = (TextView) findViewById(R.id.gyro_data);
+        light_data = (TextView) findViewById(R.id.light_data);
+        mag_data = (TextView) findViewById(R.id.mag_data);
+        stop = (Button) findViewById(R.id.stop);
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sensorManager.unregisterListener(MainActivity.this);
+            }
+        });
+        start = (Button) findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
@@ -64,9 +107,52 @@ public class MainActivity extends Activity
     @Override
     public void onDestroy() {
         super.onDestroy();
-
         unregisterReceiver(_receiver);
     }
 
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        //Util.LOGI("=========onSensorChanged===========");
+        //Util.LOGI("["+event.sensor.getName()+","+event.values+"]");
+        int type = event.sensor.getType();
+        int size = event.values.length;
+        StringBuilder sb = new StringBuilder();
+        sb = debugData(size, sb, event);
+        switch (type) {
+            case Sensor.TYPE_LINEAR_ACCELERATION:
+                acc_data.setText(sb.toString());
+                break;
+            case Sensor.TYPE_GRAVITY:
+                gra_data.setText(sb.toString());
+                break;
+            case Sensor.TYPE_GYROSCOPE:
+                gyro_data.setText(sb.toString());
+                break;
+            case Sensor.TYPE_LIGHT:
+                light_data.setText(sb.toString());
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                mag_data.setText(sb.toString());
+                break;
+        }
+    }
+
+    private StringBuilder debugData(int size, StringBuilder sb, SensorEvent event) {
+        sb.append("data size:");
+        sb.append(size);
+        sb.append("[");
+        for (int i=0;i<size;i++) {
+            float v = event.values[i];
+            sb.append(v);
+            sb.append(",");
+        }
+        sb.append("]");
+        return sb;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
